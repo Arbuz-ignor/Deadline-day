9564265c25f8aa59d9d23282eb1b6e2e7019c541
Arbuz-ignor <maksimplov@gmail.com>
2026-08-23 20:06:10 +0300

    New step

---
+++ src/app/pages/transfer-center-page/transfer-center-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-center-page',
+  imports: [],
+  templateUrl: './transfer-center-page.html',
+  styleUrl: './transfer-center-page.scss',
+})
+export class TransferCenterPage {}
