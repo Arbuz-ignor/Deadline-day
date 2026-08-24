fe82aec7e540e3a0deef983f85870a95d7188fb8
Arbuz-ignor <maksimplov@gmail.com>
2026-08-24 17:02:07 +0300

    New filter

--- src/app/pages/transfer-center-page/components/game-header/game-header.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-game-header',
-  imports: [],
-  templateUrl: './game-header.html',
-  styleUrl: './game-header.scss',
-})
-export class GameHeader {}
