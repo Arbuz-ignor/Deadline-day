871b4530e3a8c4a90e88d108eb8c2e2e45469f7c
Arbuz-ignor <maksimplov@gmail.com>
2026-08-25 16:17:16 +0300

    Input for data in center page

--- src/app/pages/transfer-center-page/components/active-deal-card/active-deal-card.ts
+++ src/app/pages/transfer-center-page/components/active-deal-card/active-deal-card.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import type { Deal } from '../../../../core/models/deal.model';
+import type { Player } from '../../../../core/models/player.model';
 @Component({
   selector: 'app-active-deal-card',
   imports: [],
   templateUrl: './active-deal-card.html',
   styleUrl: './active-deal-card.scss',
 })
-export class ActiveDealCard {}
+export class ActiveDealCard {
+  readonly player = input.required<Player>();
+  readonly deal = input.required<Deal>();
+}
--- src/app/pages/transfer-center-page/transfer-center-page.ts
+++ src/app/pages/transfer-center-page/transfer-center-page.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GameHeader } from '../../components/game-header/game-header';
-import { ActiveDealCard } from "./components/active-deal-card/active-deal-card";
-import { PlayerPhoto } from "./components/player-photo/player-photo";
-import { ScoutRadar } from "./components/scout-radar/scout-radar";
-import { TransferBoard } from "./components/transfer-board/transfer-board";
-import { EventFeed } from "./components/event-feed/event-feed";
-
+import { ActiveDealCard } from './components/active-deal-card/active-deal-card';
+import { PlayerPhoto } from './components/player-photo/player-photo';
+import { ScoutRadar } from './components/scout-radar/scout-radar';
+import { TransferBoard } from './components/transfer-board/transfer-board';
+import { EventFeed } from './components/event-feed/event-feed';
+import { GameStore } from '../../core/state/game.store';
 @Component({
   selector: 'app-transfer-center-page',
   imports: [GameHeader, ActiveDealCard, PlayerPhoto, ScoutRadar, TransferBoard, EventFeed],
   templateUrl: './transfer-center-page.html',
   styleUrl: './transfer-center-page.scss',
 })
-export class TransferCenterPage {}
+export class TransferCenterPage {
+  readonly gameStore = inject(GameStore);
+}
